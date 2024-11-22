b501f0b96f00c112b8e84f4f7a450549625b4217
OUMIF <eladnanimehdi03@gmail.com>
2024-11-22 21:01:31 +0100

    register

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import './components/RegistrationPage.css'; 
 import './App.css';
+import RegistrationPage from './components/RegistrationPage'; // Importation du composant
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RegistrationPage /> {/* Utilisation du composant RegistrationPage */}
     </div>
   );
 }
